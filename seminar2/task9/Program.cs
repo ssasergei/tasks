7dbd6053713c1a4e332b83b39e80da6c7c30fe1a
sasergei <soundboy@inbox.ru>
2023-03-10 21:39:19 +0300

    add readme task

---
+++ seminar2/task9/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 9
+// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+
+int num = new Random().Next(10, 100);
+
+Console.WriteLine(num);
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine("Наибольшая цифра числа {0} равна {1}", num, num1);
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа {num} равна {num2}");
+    Console.WriteLine("Наибольшая цифра числа " + num + " равна " + num2);
+}
+
